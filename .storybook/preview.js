22049c1b78bb47091feccca4c2c4b1fea10fa17f
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-04-27 09:57:44 -0300

    feat: create Logo Component and unit test

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,12 @@
-import { addDecorator } from '@storybook/react'
-import { withNextRouter } from 'storybook-addon-next-router'
-import GlobalStyles from '../src/styles/global'
-
-addDecorator(withNextRouter())
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ]
